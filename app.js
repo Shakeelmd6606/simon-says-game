d6db54bd6e93ef5b2dec11b1d20c8796e5208848
datla student <shakeelmd6606@gmail.com>
2023-10-05 22:54:40 +0530

    simon

---
+++ app.js
@@ -0,0 +1,90 @@
+let gameSeq=[]
+let userSeq=[]
+
+let btns=["yellow","red","purple","green"]
+
+let started=false
+let level=0
+ 
+
+document.addEventListener("keypress",function(){
+   if (started==false){
+    console.log("game is started")
+    started=true
+    levelUp();
+   }
+})
+
+
+function gameFlash(btn){
+btn.classList.add("flash")
+setTimeout(function(){
+    btn.classList.remove("flash")
+}, 250)
+}
+
+
+function userflash(btn){
+    btn.classList.add("userflash")
+    setTimeout(function(){
+        btn.classList.remove("userflash")
+    }, 250)
+    }
+
+
+function levelUp(){
+    userSeq=[]
+level ++;
+h2.innerText=`level ${level}`
+let randIdx =Math.floor(Math.random() * 3)
+let randColor =btns[randIdx]
+let randbtn=document.querySelector(`.${randColor}`)
+// console.log(randbtn)
+// console.log(randIdx)
+// console.log(randColor)
+gameSeq.push(randColor)
+ 
+gameFlash(randbtn)
+ 
+
+}
+
+ 
+function checkAns(idx){
+    if(userSeq[idx]===gameSeq[idx]){
+    if(userSeq.length==gameSeq.length){
+         setTimeout(levelUp,1000)
+          
+    }
+    }else{
+        h2.innerHTML=`game over ! your score was <b>${level}</b><br> any key to start`
+        document.querySelector("body").style.backgroundColor="red"
+        setTimeout(function(){
+            document.querySelector("body").style.backgroundColor="white"    
+        },150)
+        reset()
+    }
+}
+
+
+function btnpress(){
+   let btn = this
+    
+   userflash(btn)
+   userColor=btn.getAttribute("id")
+   userSeq.push(userColor) 
+   checkAns(userSeq.length-1)
+
+}
+
+let allBtns=document.querySelectorAll(".btn")
+for(btn of allBtns){
+    btn.addEventListener("click",btnpress)
+}
+
+function reset(){
+    started=false
+    gameSeq=[]
+    userSeq=[]
+    level=0
+}
